77731779c9135cc98b7a13034e3d7621731bac54
Andis <akacerovskis@inbox.lv>
2017-02-03 19:54:27 +0200

    .

--- js/load-footer.js
+++ js/load-footer.js
@@ -1,6 +1,7 @@
 /* Kamēr video nav ielādējies un neparādās, ir redzams fiksētais footeris aiz video*/
 
-
-$('.front, .bgvid').ready(function() {
-  $("footer").removeClass("hide");
+$(function() {
+  $('.bgvid').ready(function() {
+    $("footer").removeClass("hide");
+  });
 });
