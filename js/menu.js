e247cef7e8e03585db6c22cf73d93eda84fb7882
Andis <akacerovskis@inbox.lv>
2017-03-30 19:52:02 +0300

    Unimportant fixes

--- js/menu.js
+++ js/menu.js
@@ -2,7 +2,7 @@ $(function() {
   $("#toggle").click(function() {
     $(this).toggleClass("active");
     $('#overlay').toggleClass('open');
-    $('.blur-front, .logo-text, body, .comment-bg, .reg-wrap, .gallery, .kontakti, #map').toggleClass('active');
+    $('.blur-front, .logo-text, body, .comment-bg, .reg-wrap, .wrapp, .gallery, .kontakti, #map').toggleClass('active');
     $('.menu-reverse-color').toggleClass('reverse');
   });
 });
